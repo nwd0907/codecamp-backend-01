9541333f9a07a693b240514117ccc6e6d581e743
nwd0907 <nwd0907@newbizstart.com>
2022-01-27 08:28:01 +0900

    Update decorator

--- class/12-04-decorator/index.ts
+++ class/12-04-decorator/index.ts
@@ -1,8 +1,19 @@
-function zzz(aaaaaaa) {
-  console.log("==============");
-  console.log(aaaaaaa);
-  console.log("==============");
+// function zzz(aaaaaaa) {
+//   console.log("==============");
+//   console.log(aaaaaaa);
+//   console.log("==============");
+// }
+
+// @zzz
+// class AppController {}
+
+function zzz() {
+  return (aaaaaaa) => {
+    console.log("==============");
+    console.log(aaaaaaa);
+    console.log("==============");
+  };
 }
 
-@zzz
+@zzz()
 class AppController {}
