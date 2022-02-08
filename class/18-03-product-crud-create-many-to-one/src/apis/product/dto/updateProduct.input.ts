09952211a995b477edd11364fb820a17c94def17
nwd0907 <nwd0907@newbizstart.com>
2022-02-08 19:16:13 +0900

    Update curd-with-join

---
+++ class/18-03-product-crud-create-many-to-one/src/apis/product/dto/updateProduct.input.ts
@@ -0,0 +1,13 @@
+import { InputType, Field, Int } from '@nestjs/graphql';
+
+@InputType()
+export class UpdateProductInput {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => String)
+  description: string;
+
+  @Field(() => Int)
+  price: number;
+}
